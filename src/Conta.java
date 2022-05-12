0ed1afa6ac85543a2675ce779c9c06d9eafc6a83
Molina <83372957+klaynmolina@users.noreply.github.com>
2022-05-12 20:38:02 -0300

    Domínio/Problema

---
+++ src/Conta.java
@@ -0,0 +1,44 @@
+
+public class Conta {
+
+	private int agencia;
+	private int numeroConta;
+	private double saldo;
+
+	public static void deposito() {
+
+	}
+
+	public static void saque() {
+
+	}
+
+	public static void transferencia() {
+
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+}
